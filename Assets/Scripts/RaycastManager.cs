fe2ccd8c29be29960cc35410e42305a4a62ec69c
BuildTools <unconfigured@null.spigotmc.org>
2018-01-11 09:53:26 +0100

    Start of project

---
+++ Assets/Scripts/RaycastManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastManager : MonoBehaviour
+{
+
+    public static GameObject GetRaycastHit()
+    {
+        RaycastHit hit;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject objectHit = hit.transform.gameObject;
+            return objectHit;
+
+        }
+        return null;
+    }
+}
