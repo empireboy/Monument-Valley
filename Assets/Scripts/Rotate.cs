2e50c26d3b222dbc53db491b1845c1b608a4b533
wakamantroy <troy_toendang@hotmail.com>
2018-01-12 11:23:24 +0100

    Rotation V1

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    private bool rotatingClockwise = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotatingObjects();
+    }
+
+    void RotatingObjects()
+    {
+        if (rotatingClockwise && transform.eulerAngles.z < 90)
+        {
+            transform.Rotate(0, 0, 1);
+        }
+        else if (rotatingClockwise)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+
+        if (!rotatingClockwise && transform.eulerAngles.z >= 1)
+        {
+            transform.Rotate(0, 0, -1);
+        }
+        else if (!rotatingClockwise)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        if (Input.anyKeyDown)
+        {
+            rotatingClockwise = !rotatingClockwise;
+        }
+    }
+}
