d22aeff1b43e8a28bf18d25f93e1e6bf18e569a5
BuildTools <unconfigured@null.spigotmc.org>
2018-01-21 16:27:11 +0100

    Path Managing V4

--- Assets/Scripts/NodeInitialize.cs
+++ Assets/Scripts/NodeInitialize.cs
@@ -3,4 +3,6 @@
 public class NodeInitialize : MonoBehaviour {
     public GameObject[] nodeConnectionsArray;
     public bool playerHere = false;
+    public bool isChecked = false;
+    public int pathStartIndex = -1;
 }
--- Assets/Scripts/PathController.cs
+++ Assets/Scripts/PathController.cs
@@ -5,6 +5,7 @@ public class PathController : MonoBehaviour {
     private GameObject _nextNode;
     //private List<GameObject> _nodeArrayFinished = new List<GameObject>();
     private List<List<GameObject>> _nodeArrayFinished = new List<List<GameObject>>();
+    private List<int> _pathSuccesIndexArray = new List<int>();
     private bool _pathCheckFinished = false;
     private bool _pathsCheckFinished = false;
     private bool _isAnalyzing = false;
@@ -72,6 +73,7 @@ public class PathController : MonoBehaviour {
         {
             if (_debug) Debug.Log("Path finiding failed for all paths");
             _pathsCheckFinished = true;
+            _isAnalyzing = false;
         }
         else
         // Iterate trough all paths again
@@ -82,8 +84,14 @@ public class PathController : MonoBehaviour {
 
     private void GetPath(Vector3 playerPos, GameObject endObject, int index)
     {
-        GameObject checkObject = endObject;
-        _nodeArrayFinished[index].Add(checkObject);
+        if (IsSucceeded(index)) { PathReset(); return; }        // Return if this path already succeeded to find a player
+
+        // Get the path start index if exists
+        GameObject checkObject = GetPathStartIndex(index);
+        if (checkObject == null) checkObject = endObject;
+
+        _nodeArrayFinished[index].Add(checkObject);         // Add the end node to the array
+        checkObject.GetComponent<NodeInitialize>().isChecked = true;        // Set the isChecked bool to true in the end node so it won't be checked later
         if (_debug) Debug.Log("End node: " + checkObject);
         
         while (!_pathCheckFinished)
@@ -94,17 +102,21 @@ public class PathController : MonoBehaviour {
             for (int i = 0; i < nodeConnectionsArray.Length; i++)
             {
                 GameObject currentNode = nodeConnectionsArray[i];
-                
-                if (_nextNode == null)
+                NodeInitialize currentNodeInit = currentNode.GetComponent<NodeInitialize>();
+
+                if (_nextNode == null && !currentNodeInit.isChecked)
                 {
                     _nextNode = currentNode;
+                    currentNodeInit.isChecked = true;
                 }
-                else
+                else if (_nextNode != null && !currentNodeInit.isChecked)
                 {
                     if (_totalPathsCountdown > 0)
                     {
-                        Debug.Log("New path created on a node: " + currentNode);
-                        _nodeArrayFinished.Add(new List<GameObject>());         // Create a new path
+                        if (_debug) Debug.Log("New path created on a node: " + currentNode);
+                        _nodeArrayFinished.Add(new List<GameObject>());         // Create new path
+                        currentNodeInit.pathStartIndex = _nodeArrayFinished.Count-1;      // Give the current path index to the current node
+                        if (_debug) Debug.Log("Given the node: " + currentNode + " a pathStartIndex: " + currentNodeInit.pathStartIndex);
                     }
                     _totalPathsCountdown--;
                 }
@@ -112,15 +124,24 @@ public class PathController : MonoBehaviour {
             
             if (_debug) Debug.Log("Next node to check: " + _nextNode);
 
-            checkObject = _nextNode;
+            if (_nextNode != null)
+            {
+                checkObject = _nextNode;
+            }
+            else
+            {
+                PathReset();
+                return;
+            }
 
-            _nodeArrayFinished[index].Add(checkObject);            // Add the next node the the finished node array
+            _nodeArrayFinished[index].Add(checkObject);            // Add the next node to the finished node array
 
             // Found a path
             if (checkObject.GetComponent<NodeInitialize>().playerHere)
             {
                 if (_debug) Debug.Log("Path finding succes for index: " + index);
                 _pathCheckFinished = true;
+                _pathSuccesIndexArray.Add(index);       // Add the index of the current array to an array that keeps track of all paths that succeed
                 //GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().PathCreate(_nodeArrayFinished);
             }
 
@@ -138,7 +159,38 @@ public class PathController : MonoBehaviour {
         }
 
         // Reset some variables after each path check
+        PathReset();
+    }
+
+    private bool IsSucceeded(int index)
+    {
+        for (int i = 0; i < _pathSuccesIndexArray.Count; i++)
+        {
+            if (_pathSuccesIndexArray[i] == index)
+            {
+                if (_debug) Debug.Log("The current path with index: " + index + " already succeeded to find a player");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void PathReset()
+    {
         _pathCountdown = _countdownMax;
         _pathCheckFinished = false;
+    }
+
+    private GameObject GetPathStartIndex(int index)
+    {
+        GameObject[] objectsWithTag = GameObject.FindGameObjectsWithTag("Path");
+
+        for (int i = 0; i < objectsWithTag.Length; i++)
+        {
+            NodeInitialize currentNodeInit = objectsWithTag[i].GetComponent<NodeInitialize>();
+            if (currentNodeInit.pathStartIndex == index) return objectsWithTag[i];
+        }
+
+        return null;
     }
 }
