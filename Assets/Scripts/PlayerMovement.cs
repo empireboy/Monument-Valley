c193435a7195d3c079b6c224a7cad0f62202d73a
BuildTools <unconfigured@null.spigotmc.org>
2018-02-03 00:11:17 +0100

    Final result

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -145,7 +145,7 @@ public class PlayerMovement : MonoBehaviour {
         switch (_moveState)
         {
             case _moveStateEnum.idle:
-                lockToNode(_path[_currentNodeCounter + 1]);
+                //lockToNode(_path[_currentNodeCounter + 1]);
                 break;
             case _moveStateEnum.walk:
                 if (_directionState == _directionStateEnum.noDirection) GetDirection(_path[_currentNodeCounter], _path[_currentNodeCounter+1]);
